3b2e6b4424cc876aad2c9139e0c5b7ac550ca184
Eria Othieno Pinyi <eria.pinyi@crst.com>
2023-05-31 17:10:07 -0400

    Best router practices and configuration

--- angular-router-course-1-start/src/app/courses/courses-routing.module.ts
+++ angular-router-course-1-start/src/app/courses/courses-routing.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CourseComponent } from './course/course.component';
 
 
 const routes: Routes = [
-
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: ":couseUrl",
+    component: CourseComponent
+  }
 ];
 
 @NgModule({
   imports: [
-
+    RouterModule.forChild(routes)
   ],
   exports: [RouterModule],
   providers: [
